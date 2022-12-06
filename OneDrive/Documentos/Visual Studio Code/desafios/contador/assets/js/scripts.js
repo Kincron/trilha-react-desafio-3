7b442861dd13167b6b07fd8e36a96bfa89fe44e9
Kincron <rafaelfelipesoares192@gmail.com>
2022-12-06 13:22:51 -0300

    First commit

---
+++ OneDrive/Documentos/Visual Studio Code/desafios/contador/assets/js/scripts.js
@@ -0,0 +1,27 @@
+
+var currentNumberWrapper = document.getElementById('currentNumber')
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+
+    if (currentNumber >= 10) {
+        currentNumberWrapper.style.color = 'red'
+    } else {
+        currentNumberWrapper.style.color = 'black'
+    }
+
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    
+     if (currentNumber < 0) {
+        currentNumberWrapper.style.color = 'red'
+     } else {
+        currentNumberWrapper.style.color = 'black'
+     }
+}
+
